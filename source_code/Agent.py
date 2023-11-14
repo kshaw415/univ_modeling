34f3f41c211cec9fb1554d37ecccf73749a823c8
Kikuyo Shaw <kikuyo_shaw@brown.edu>
2023-11-14 00:02:24 -0500

    first commit

---
+++ source_code/Agent.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+class Agent: 
+    '''
+    class for individual agents. 
+
+    Fields: 
+
+    
+    '''
+
+    def __init__(self, id: int, infected: int): 
+        '''
+        Initialization method for Agent class
+
+        Fields: 
+            id --> int, unique agent ID
+            infected --> int, 0 if Susceptible, 1 if exposed, 2 if infected
+        '''
+        self.id = 0; # CHANGE TO MAKE UNIQUE 
+        self.infected = 0; # CHANGE 
+
+
